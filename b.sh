01ee54b60b458f92eff526370e05830661f39caf
sasaplus1 <sasaplus1@gmail.com>
2025-01-30 15:32:00 +0900

    fix

--- b.sh
+++ b.sh
@@ -6,7 +6,7 @@ set -uo pipefail
 __main() {
   set -x
 
-  printf -- '%s\n' 0 1 0 | xargs -P 0 -n 1 -I % bash -c 'exit %'
+  printf -- '%s\n' 0 1 0 | xargs -P 0 -I % bash -c 'echo %; exit %'
   printf -- '$?: %s\n' "$?"
 
   set +x

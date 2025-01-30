01318905eac7f691347f415bf03bcd43a0f6f5df
sasaplus1 <sasaplus1@gmail.com>
2025-01-30 15:33:57 +0900

    fix

--- a.sh
+++ a.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 __main() {
   set -x
 
-  printf -- '%s\n' 1 2 3 | xargs -P 0 -n 1 -I % bash -c 'echo %'
+  printf -- '%s\n' 1 2 3 | xargs -P 0 -I % bash -c 'echo %'
   printf -- '$?: %s\n' "$?"
 
   set +x
--- b.sh
+++ b.sh
@@ -7,8 +7,11 @@ __main() {
   set -x
 
   printf -- '%s\n' 0 1 0 | xargs -P 0 -I % bash -c 'echo %; exit %'
+  local -r exit_code="$?"
   printf -- '$?: %s\n' "$?"
 
   set +x
+
+  exit "$exit_code"
 }
 __main "$@"

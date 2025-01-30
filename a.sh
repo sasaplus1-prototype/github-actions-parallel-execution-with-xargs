393259594c4a33bffecc8df94b6479c433a0190f
sasaplus1 <sasaplus1@gmail.com>
2025-01-30 15:26:17 +0900

    fix

--- a.sh
+++ a.sh
@@ -5,9 +5,7 @@ set -euo pipefail
 __main() {
   set -x
 
-  local script=()
-
-  printf -- 'echo %s\n' 1 2 3 | xargs -P 0 -n 1 -I % bash -c
+  printf -- '%s\n' 1 2 3 | xargs -P 0 -n 1 -I % bash -c 'echo %'
   echo "$$?: $?"
 
   set +x
